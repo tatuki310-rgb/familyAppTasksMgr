bc4e2b78183eb4c688f69d01bab22c41d2865688
tatuki310-rgb <tatuki310@gmail.com>
2026-03-19 22:41:19 +0900

    Update DynamoDbConfig.java region

--- src/main/java/com/example/taskmanager/config/DynamoDbConfig.java
+++ src/main/java/com/example/taskmanager/config/DynamoDbConfig.java
@@ -13,7 +13,7 @@ public class DynamoDbConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1) // Use your desired region here
+                .region(Region.AP_NORTHEAST_1) // Use your desired region here
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
